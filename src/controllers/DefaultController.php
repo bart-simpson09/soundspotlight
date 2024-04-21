f3fb5ac6f8a12b4350143c5b5fd374dd3d413cf3
Mateusz Sajdak <sajdak.mateusz.219@gmail.com>
2024-04-21 15:51:43 +0200

    routing v1 finished

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -7,36 +7,8 @@ class DefaultController extends AppController {
     public function index() {
         $this->render('login');
     }
-
-    public function register() {
-        $this->render('register');
-    }
     
     public function dashboard() {
         $this->render('dashboard');
-    }
-
-    public function addAlbum() {
-        $this->render('addAlbum');
-    }
-
-    public function adminConsole() {
-        $this->render('adminConsole');
-    }
-
-    public function albumDetails() {
-        $this->render('albumDetails');
-    }
-
-    public function myProfile() {
-        $this->render('myProfile');
-    }
-
-    public function topAlbums() {
-        $this->render('topAlbums');
-    }
-
-    public function yourFavorite() {
-        $this->render('yourFavorite');
     }
 }
