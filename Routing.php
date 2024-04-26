cb2067917de1554bf8baf2c129428b0da030c216
Mateusz Sajdak <sajdak.mateusz.219@gmail.com>
2024-04-26 13:01:07 +0200

    splited views to public and protected

--- Routing.php
+++ Routing.php
@@ -4,28 +4,43 @@ require_once 'src/controllers/DefaultController.php';
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/DashboardController.php';
 
-class Router {
+class Router
+{
 
-  public static $routes;
+    public static $routes;
 
-  public static function get($url, $view) {
-    self::$routes[$url] = $view;
-  }
-
-  public static function post($url, $view) {
-    self::$routes[$url] = $view;
-  }
-
-  public static function run ($url) {
-    $action = explode("/", $url)[0];
-    if (!array_key_exists($action, self::$routes)) {
-      die("Wrong url!");
+    public static function get($url, $view)
+    {
+        self::$routes[$url] = $view;
     }
 
-    $controller = self::$routes[$action];
-    $object = new $controller;
-    $action = $action ?: 'index';
+    public static function post($url, $view)
+    {
+        self::$routes[$url] = $view;
+    }
 
-    $object->$action();
-  }
+    public static function run($url)
+    {
+        $action = explode("/", $url)[0];
+        if (!array_key_exists($action, self::$routes)) {
+            die("Wrong url!");
+        }
+
+        $userSession = SessionManager::getInstance();
+        $userId = $userSession->__get("userId");
+        $publicPages = ['login', 'register'];
+
+        if (!$userId && !in_array($action, $publicPages)) {
+            header("Location: /login");
+            exit();
+        } else if ($userId && in_array($action, $publicPages)) {
+            header("Location: /dashboard");
+            exit();
+        }
+
+        $controller = self::$routes[$action];
+        $object = new $controller;
+        $action = $action ?: 'index';
+        $object->$action();
+    }
 }
