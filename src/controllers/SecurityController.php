74e238aa001bd46d3a336e6f65cd30fca5ef22d3
Mateusz Sajdak <sajdak.mateusz.219@gmail.com>
2024-05-18 14:16:32 +0200

    SecurityController.php refactored

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -7,7 +7,6 @@ require_once __DIR__ . '/../repository/UserRepository.php';
 
 class SecurityController extends AppController
 {
-
     private $userRepository;
 
     public function __construct()
@@ -20,19 +19,31 @@ class SecurityController extends AppController
     {
         $userSession = SessionManager::getInstance();
         $userId = $userSession->__get("userId");
-        $url = "http://$_SERVER[HTTP_HOST]";
+        $url = "http://{$_SERVER['HTTP_HOST']}";
 
-        if ($userId == null) {
-            header("Location: $url/login");
+        if ($userId === null) {
+            $this->redirectToLogin();
         } else {
-            header("Location: $url/dashboard");
+            $this->redirectToDashboard();
         }
+    }
+
+    private function redirectToLogin()
+    {
+        $url = "http://{$_SERVER['HTTP_HOST']}/login";
+        header("Location: $url");
+        exit();
+    }
+
+    private function redirectToDashboard()
+    {
+        $url = "http://{$_SERVER['HTTP_HOST']}/dashboard";
+        header("Location: $url");
         exit();
     }
 
     public function login()
     {
-
         if (!$this->isPost()) {
             print $this->render('/login');
             return;
@@ -57,8 +68,7 @@ class SecurityController extends AppController
         $userSession->__set('userId', $user->getId());
         $userSession->__set('userEmail', $user->getEmail());
 
-        $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: $url/dashboard");
+        $this->redirectToDashboard();
     }
 
     public function logout()
@@ -66,8 +76,7 @@ class SecurityController extends AppController
         $userSession = SessionManager::getInstance();
         $userSession->destroySession();
 
-        $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: $url/");
+        $this->redirectToLogin();
     }
 
     public function register()
@@ -85,6 +94,32 @@ class SecurityController extends AppController
         $password = $_POST['password'];
         $repeatedPassword = $_POST['repeatedPassword'];
 
+        $this->validateRegisterForm($firstName, $lastName, $email, $password, $repeatedPassword, $handledErrors);
+
+        $user = $this->userRepository->getUser($email);
+
+        if ($user !== null && $email === $user->getEmail()) {
+            $handledErrors['userExists'] = "User with this email address already exists";
+        }
+
+        if (!empty($handledErrors)) {
+            print $this->render('/register', ['messages' => $handledErrors]);
+            return;
+        }
+
+        $addUser = new User(null, $email, password_hash($password, PASSWORD_BCRYPT), $firstName, $lastName, null, null);
+        $this->userRepository->addUser($addUser);
+
+        $user = $this->userRepository->getUser($email);
+        $userSession = SessionManager::getInstance();
+        $userSession->__set('userId', $user->getId());
+        $userSession->__set('userEmail', $user->getEmail());
+
+        $this->redirectToLogin();
+    }
+
+    private function validateRegisterForm($firstName, $lastName, $email, $password, $repeatedPassword, &$handledErrors)
+    {
         if (empty($firstName)) {
             $handledErrors['firstName'] = "First name is required";
         }
@@ -105,32 +140,8 @@ class SecurityController extends AppController
             $handledErrors['repeatedPassword'] = "Type your password again";
         }
 
-        if ($password != $repeatedPassword) {
+        if ($password !== $repeatedPassword) {
             $handledErrors['repeatedPassword'] = "Repeated password does not match";
         }
-
-        $user = $this->userRepository->getUser($email);
-
-        if ($user != null) {
-            if ($email == $user->getEmail()) {
-                $handledErrors['userExists'] = "User with this email address already exists";
-            }
-        }
-
-        if (!empty($handledErrors)) {
-            print $this->render('/register', ['messages' => $handledErrors]);
-            return;
-        }
-
-        $addUser = new User(null, $email, password_hash($password, PASSWORD_BCRYPT), $firstName, $lastName, null, null);
-        $this->userRepository->addUser($addUser);
-
-        $user = $this->userRepository->getUser($email);
-        $userSession = SessionManager::getInstance();
-        $userSession->__set('userId', $user->getId());
-        $userSession->__set('userEmail', $user->getEmail());
-
-        $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: $url/");
     }
 }
