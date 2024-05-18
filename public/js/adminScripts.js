e8631bdf560042db6bd1258cf3816f059b668a5a
Mateusz Sajdak <sajdak.mateusz.219@gmail.com>
2024-05-18 12:37:21 +0200

    adminScripts.js refactored

--- public/js/adminScripts.js
+++ public/js/adminScripts.js
@@ -2,198 +2,119 @@ const pendingReviewsContainer = document.querySelector("#pendingReviews");
 const pendingAlbumsContainer = document.querySelector("#pendingAlbums");
 const manageUsersContainer = document.querySelector("#manageUsers");
 
-function reviewOpinion(decision, reviewId) {
-    const data = {
-        decision: decision,
-        reviewId: reviewId
-    };
-
-    fetch("/changeReviewStatus", {
+const postRequest = (url, data) => {
+    return fetch(url, {
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
-    }).then(function (response) {
-        return response.json();
-    }).then(function (reviews) {
-        pendingReviewsContainer.innerHTML = "";
-        loadReviews(reviews);
-    })
+    }).then(response => response.json());
 }
 
-function loadReviews(reviews) {
-    if (reviews.length > 0) {
-        reviews.forEach(review => {
-            createReview(review);
-        });
-    } else {
-        pendingReviewsContainer.innerHTML = `<p>There are no pending reviews at the moment.</p>`;
-    }
+const updateContainer = (container, data, loadFunction, emptyMessage) => {
+    container.innerHTML = data.length > 0 ? '' : `<p>${emptyMessage}</p>`;
+    data.forEach(item => loadFunction(item));
 }
 
-function createReview(review) {
+function reviewOpinion(decision, reviewId) {
+    const data = {decision, reviewId};
+    postRequest("/changeReviewStatus", data)
+        .then(reviews => updateContainer(pendingReviewsContainer, reviews, createReview, "There are no pending reviews at the moment."));
+}
+
+function reviewAlbum(decision, albumId) {
+    const data = {decision, albumId};
+    postRequest("/changeAlbumStatus", data)
+        .then(albums => updateContainer(pendingAlbumsContainer, albums, createAlbum, "There are no pending albums at the moment."));
+}
+
+function manageUser(decision, userId, loggedUserId) {
+    const data = {decision, userId};
+    postRequest("/manageUser", data)
+        .then(users => updateContainer(manageUsersContainer, users, user => createUser(user, loggedUserId), ""));
+}
+
+const createReview = review => {
     const template = document.querySelector("#reviewTemplate");
-
     const clone = template.content.cloneNode(true);
 
-    const albumName = clone.querySelector("#rAlbumName");
-    albumName.innerHTML = review.albumname;
+    setTextContent(clone, "#rAlbumName", review.albumname);
+    setTextContent(clone, "#rAlbumAuthorName", review.albumauthorname);
+    setInnerHTML(clone, "#rOpinionRate", `${review.rate}/5 <i class="iconoir-star-solid"></i>`);
+    setTextContent(clone, "#rContent", review.content);
+    setTextContent(clone, "#rAuthorName", `${review.authorfirstname} ${review.authorlastname}`);
 
-    const albumAuthorName = clone.querySelector("#rAlbumAuthorName");
-    albumAuthorName.innerHTML = review.albumauthorname;
-
-    const opinionRate = clone.querySelector("#rOpinionRate");
-    opinionRate.innerHTML = review.rate + "/5";
-    opinionRate.innerHTML += `<i class="iconoir-star-solid"></i>`;
-
-    const actionButtons = clone.querySelector("#rActionButtons");
-    actionButtons.innerHTML = `<button class="buttonOutlined positiveAction"
-                            onclick="reviewOpinion('Approve', ${review.id})">Approve
-                    </button>`;
-    actionButtons.innerHTML += `<button class="buttonOutlined importantAction"
-                            onclick="reviewOpinion('Decline', ${review.id})">Decline
-                    </button>`;
-
-    const content = clone.querySelector("#rContent");
-    content.innerHTML = review.content;
-
-    const authorName = clone.querySelector("#rAuthorName");
-    authorName.innerHTML = review.authorfirstname + " " + review.authorlastname;
+    setInnerHTML(clone, "#rActionButtons", `
+        <button class="buttonOutlined positiveAction" onclick="reviewOpinion('Approve', ${review.id})">Approve</button>
+        <button class="buttonOutlined importantAction" onclick="reviewOpinion('Decline', ${review.id})">Decline</button>
+    `);
 
     pendingReviewsContainer.appendChild(clone);
 }
 
-function reviewAlbum(decision, albumId) {
-    const data = {
-        decision: decision,
-        albumId: albumId
-    };
-
-    fetch("/changeAlbumStatus", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(function (response) {
-        return response.json();
-    }).then(function (albums) {
-        pendingAlbumsContainer.innerHTML = "";
-        loadAlbums(albums);
-    })
-}
-
-function loadAlbums(albums) {
-    if (albums.length > 0) {
-        albums.forEach(album => {
-            createAlbum(album);
-        });
-    } else {
-        pendingAlbumsContainer.innerHTML = `<p>There are no pending albums at the moment.</p>`;
-    }
-}
-
-function createAlbum(album) {
+const createAlbum = album => {
     const template = document.querySelector("#albumTemplate");
-
     const clone = template.content.cloneNode(true);
 
-    const albumCover = clone.querySelector("img");
-    albumCover.src = `/public/assets/imgs/covers/${album.cover}`;
+    setSrc(clone, "img", `/public/assets/imgs/covers/${album.cover}`);
+    setTextContent(clone, "#aAlbumName", album.albumtitle);
+    setTextContent(clone, "#aAlbumAuthorName", album.authorname);
+    setTextContent(clone, "#aCategoryName", album.categoryname);
+    setTextContent(clone, "#aLanguage", album.languagename);
+    setTextContent(clone, "#aReleaseDate", album.releasedate);
+    setTextContent(clone, ".shortAlbumDescription", album.description);
+    setTextContent(clone, "#aAuthorName", `${album.userfirstname} ${album.userlastname}`);
 
-    const albumName = clone.querySelector("#aAlbumName");
-    albumName.innerHTML = album.albumtitle;
-
-    const albumAuthorName = clone.querySelector("#aAlbumAuthorName");
-    albumAuthorName.innerHTML = album.authorname;
-
-    const actionButtons = clone.querySelector("#aActionButtons");
-    actionButtons.innerHTML = `<button class="buttonOutlined positiveAction"
-                            onclick="reviewAlbum('Approve', ${album.id})">Approve
-                    </button>`;
-    actionButtons.innerHTML += `<button class="buttonOutlined importantAction"
-                            onclick="reviewAlbum('Decline', ${album.id})">Decline
-                    </button>`;
-
-    const category = clone.querySelector("#aCategoryName");
-    category.innerHTML = album.categoryname;
-
-    const language = clone.querySelector("#aLanguage");
-    language.innerHTML = album.languagename;
-
-    const releaseDate = clone.querySelector("#aReleaseDate");
-    releaseDate.innerHTML = album.releasedate;
-
-    const albumDescription = clone.querySelector(".shortAlbumDescription");
-    albumDescription.innerHTML = album.description;
-
-    const authorName = clone.querySelector("#aAuthorName");
-    authorName.innerHTML = album.userfirstname + " " + album.userlastname;
+    setInnerHTML(clone, "#aActionButtons", `
+        <button class="buttonOutlined positiveAction" onclick="reviewAlbum('Approve', ${album.id})">Approve</button>
+        <button class="buttonOutlined importantAction" onclick="reviewAlbum('Decline', ${album.id})">Decline</button>
+    `);
 
     pendingAlbumsContainer.appendChild(clone);
 }
 
-function manageUser(decision, userId, loggedUserId) {
-    const data = {
-        decision: decision,
-        userId: userId
-    };
-
-    fetch("/manageUser", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(function (response) {
-        return response.json();
-    }).then(function (users) {
-        manageUsersContainer.innerHTML = "";
-        loadUsers(users, loggedUserId);
-    })
-}
-
-function loadUsers(users, loggedUserId) {
-    users.forEach(user => {
-        createUser(user, loggedUserId);
-    });
-}
-
-function createUser(user, loggedUserId) {
+const createUser = (user, loggedUserId) => {
     const template = document.querySelector("#userTemplate");
-
     const clone = template.content.cloneNode(true);
 
-    const userAvatar = clone.querySelector("img");
-    userAvatar.src = `/public/assets/imgs/avatars/${user.avatar}`;
+    setSrc(clone, "img", `/public/assets/imgs/avatars/${user.avatar}`);
+    setTextContent(clone, "#uUserName", `${user.firstname} ${user.lastname}`);
+    setTextContent(clone, "#userEmail", user.email);
 
-    const userName = clone.querySelector("#uUserName");
-    userName.innerHTML = user.firstname + " " + user.lastname;
-
+    const userNameSection = clone.querySelector("#userNameSection");
+    userNameSection.innerHTML = `${user.firstname} ${user.lastname}`;
     if (user.role === "admin") {
-        const userNameSection = clone.querySelector("#userNameSection");
-        userNameSection.innerHTML += `<i class="iconoir-user-crown"></i>`;
+        userNameSection.innerHTML += ` <i class="iconoir-user-crown"></i>`;
     }
-
-    const userEmail = clone.querySelector("#userEmail");
-    userEmail.innerHTML = user.email;
 
     const userActions = clone.querySelector("#userActions");
     if (user.id !== loggedUserId) {
-        if (user.role === "admin") {
-            userActions.innerHTML = `<button class="buttonOutlined"
-                                            onclick="manageUser('removeAdmin', ${user.id}, ${loggedUserId})">
-                                        Revoke admin role
-                                    </button>`;
-        } else {
-            userActions.innerHTML = `<button class="buttonOutlined" onclick="manageUser('addAdmin', ${user.id}, ${loggedUserId})"> Grant admin role</button>`;
-
-        }
-        userActions.innerHTML += `<button class="buttonOutlined importantAction" onclick="manageUser('deleteUser', ${user.id}, ${loggedUserId})">Remove user</button>`;
+        userActions.innerHTML = `
+            <button class="buttonOutlined" onclick="manageUser('${user.role === "admin" ? 'removeAdmin' : 'addAdmin'}', ${user.id}, ${loggedUserId})">
+                ${user.role === "admin" ? 'Revoke admin role' : 'Grant admin role'}
+            </button>
+            <button class="buttonOutlined importantAction" onclick="manageUser('deleteUser', ${user.id}, ${loggedUserId})">Remove user</button>
+        `;
     } else {
         userActions.innerHTML = `<h5>This is your account</h5>`;
     }
 
     manageUsersContainer.appendChild(clone);
+}
+
+const setSrc = (clone, selector, src) => {
+    const element = clone.querySelector(selector);
+    if (element) element.src = src;
+    else console.error(`${selector} element not found`);
+}
+
+const setTextContent = (clone, selector, text) => {
+    const element = clone.querySelector(selector);
+    if (element) element.textContent = text;
+    else console.error(`${selector} element not found`);
+}
+
+const setInnerHTML = (clone, selector, html) => {
+    const element = clone.querySelector(selector);
+    if (element) element.innerHTML = html;
+    else console.error(`${selector} element not found`);
 }
